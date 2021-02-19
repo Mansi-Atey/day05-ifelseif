85dd412ff5da39bb404c60f5f8cd2b40e03d5777
unknown <you@example.com>
2021-02-19 20:27:45 +0530

    Added: main and max of arithmetic operations

--- ifelseif.sh
+++ ifelseif.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-echo "enter a number"
-read num
-num=$num
-if [$num/1000]
- then
-    echo "Thousands"
- elif [$num/100]
-  then
-     echo "Hundreds"
- elif [$num/10]
-   then
-     echo "Tens"
-  else
-     echo "Unit"
+read –p “Enter First Number.” a
+     read –p “Enter Second Number.” b
+     Read –p “Enter Third Number.” C
+T1=$(($(($a + $b)) * $c))
+T2=$(($(($c + $a)) / $b))
+T3=$(($(($a % $b)) + $c))
+echo $T1
+echo $T2
+echo $T3     
+if [ $T1 –ge $T2 ] && [ $T1 –ge $T3 ]
+then 
+       echo “T1 is greater”
+elif [ $T2 –ge $T3 ] && [ $T2 –ge $T1 ]
+then 
+     echo “T2 is greater”
+elif [ $T3 –ge $T1 ] && [ $T3 –ge $T2 ]
+then 
+     echo “T3 is greater”
+else
+     echo “pass”
 fi
+
 
